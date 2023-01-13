9e896f1f875de8c3bd8f0275ac54ae8ee6b87853
Lilian Cunha <liliancunha.dev@gmail.com>
2023-01-12 21:52:17 -0300

    Rota que retorna todos os clientes e rota que cria um novo cliente

---
+++ backend-laravel/app/Http/Controllers/ClientController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Client;
+use Illuminate\Support\Facades\Validator;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'cpf' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Erro de validação', $validator->errors());
+        }
+
+        $client = Client::create($input);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Cliente criado com sucesso',
+            'client' => $client,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\ClientResource;
+use App\Models\Client;
+use App\Http\Controllers\ClientController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+Route::get('/clients', function(){
+    return ClientResource::collection(Client::all());
+});
+
+Route::get('/client/{id}', function($id) {
+    return new ClientResource(Client::findOrFail($id));
+});
+
+Route::post('/client', [ClientController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
